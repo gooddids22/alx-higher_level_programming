1c03fb7a6d49bae3fc6e5fdf2a31f996808eea3e
gooddids22 <splendidotamiri@gmail.com>
2023-06-14 06:15:09 -0700

    python.h

--- 0x04-python-more_data_structures/Python.h
+++
@@ -1,7 +0,0 @@
-#ifndef PYTHON_H
-#define PYTHON_H
-
-void print_python_list(PyObject *p);
-void print_python_bytes(PyObject *p);
-
-#endif
---
+++ 0x04-python-more_data_structures/python.h
@@ -0,0 +1,11 @@
+#ifndef PYTHON_H
+#define PYTHON_H
+
+#include <stdio.h>
+#include <Python.h>
+
+void print_python_bytes(PyObject *p);
+void print_python_list(PyObject *p);
+
+#endif /* PYTHON_H */
+
